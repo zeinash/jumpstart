f59d479004da1071106023ae9dd6472435fd40a4
zeinash <shaaban.zeina@gmail.com>
2018-06-05 01:18:36 +0300

    Carousel

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
