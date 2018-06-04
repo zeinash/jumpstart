905e8a1d70d2763606fe467b32f411b8f361fda0
zeinash <shaaban.zeina@gmail.com>
2018-06-05 02:12:50 +0300

    Hide Navigation Bar

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,4 +8,17 @@ $(document).ready(function () {
     pauseOnFocus: false,
     pauseOnHover: false
   });
+
+  var $scrollProgress = 0;
+  $(window).scroll(function (){
+    var $currentScrollPosition = $(this).scrollTop();
+    if ($currentScrollPosition - $scrollProgress > 50) {
+        var $navbarHeight = $('.navbar').css('height');
+        $('.navbar').animate( {top: '-' + $navbarHeight}, 150);
+        $scrollProgress = $currentScrollPosition;
+      } else if ($scrollProgress - $currentScrollPosition > 50) {
+        $('.navbar').animate( {top: 0}, 150);
+        $scrollProgress = $currentScrollPosition;
+      }
+    });
 });
